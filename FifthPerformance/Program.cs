8b6e78257689f2e37c1780a6259a5be755e5b2c7
Eduardo de Souza <eduardohsfreitas86@gmail.com>
2018-11-06 07:45:49 -0300

    Fifth Performance/Program made in C#

---
+++ FifthPerformance/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace FifthPerformance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int LagX, AltY, FotoX, FotoY;
+            string[] vet;
+
+            vet = Console.ReadLine().Split(' ');
+            LagX = int.Parse(vet[0]);
+            AltY = int.Parse(vet[1]);
+
+            vet = Console.ReadLine().Split(' ');
+            FotoX = int.Parse(vet[0]);
+            FotoY = int.Parse(vet[1]);
+
+            if ((FotoX <= LagX && FotoY <= AltY) || (FotoX <= LagX && FotoY <= AltY))
+            {
+                Console.WriteLine("A Foto cabe.");
+            }
+            else
+            {
+                Console.WriteLine("Vai ter que comprar uma moldura nova, parceiro.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
